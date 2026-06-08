8bce280350fd2122ab071dfead748fc8309a173b
tahyd <ta.hyd.003@gmail.com>
2026-06-08 20:08:32 +0530

    second commit commit

--- src/main/java/com/hsbc/ems/controllers/EmployeeController.java
+++ src/main/java/com/hsbc/ems/controllers/EmployeeController.java
@@ -1,12 +1,13 @@
 package com.hsbc.ems.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EmployeeController {
-    @GetMapping("/hello")
-    public String hello(){
+    @GetMapping("/hello/{name}")
+    public String hello(@PathVariable("name") String name){
         return "Hello World !";
     }
 }
