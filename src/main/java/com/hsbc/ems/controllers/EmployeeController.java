4857a3f1469945b8ad2c1c1cd5bcbb1b73471620
tahyd <ta.hyd.003@gmail.com>
2026-06-08 17:01:39 +0530

    first commit

---
+++ src/main/java/com/hsbc/ems/controllers/EmployeeController.java
@@ -0,0 +1,12 @@
+package com.hsbc.ems.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployeeController {
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello World !";
+    }
+}
